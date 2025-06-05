d7ea096f695e4e1251c83cf17d2e54230cebe242
GHDB ASHEN <115166357+GHDBASHEN@users.noreply.github.com>
2025-06-05 15:38:23 +0530

    new webapp

---
+++ CRUD App/webapp/src/test/java/com/ruhict/webapp/WebappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ruhict.webapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebappApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
